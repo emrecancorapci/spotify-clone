f2f40995017d8d02882012c02b9d33c305381482
Emre Can Çorapçı <emrecancorapci@gmail.com>
2023-11-04 00:35:45 +0300

    add shadcn/ui and slider component

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,23 +1,45 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  mode: 'jit',
+  darkMode: ["class"],
   content: [
     "./index.html",
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
     './src/**/*.{ts,tsx}',
-  ],
+	],
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
       keyframes: {
+        "accordion-down": {
+          from: { height: 0 },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: 0 },
+        },
         wiggle: {
           "0%, 50%, 100%": { transform: "rotate(-7deg)" },
           "25%, 75%": { transform: "rotate(7deg)" }
         }
+
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
       animation: {
         wiggle: "wiggle 300ms ease-in-out"
       }
     },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 }
-
