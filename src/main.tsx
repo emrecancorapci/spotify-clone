93425cf1746b62c028e55056c8e9f9bf37a89969
Emre Can Çorapçı <emrecancorapci@gmail.com>
2023-11-04 19:04:22 +0300

    add tooltip

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './global.css';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './router.tsx';
+import { TooltipProvider } from './components/ui/tooltip.tsx';
 
 ReactDOM.createRoot(document.querySelector('#root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <TooltipProvider>
+      <RouterProvider router={router} />
+    </TooltipProvider>
   </React.StrictMode>,
 );
