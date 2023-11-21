3588aea201404f4e714a3a76a4e9ea49a8d18f76
Emre Can Çorapçı <emrecancorapci@gmail.com>
2023-11-21 22:31:00 +0300

    add time bar movement

--- src/components/layout/footer/player-controller.tsx
+++ src/components/layout/footer/player-controller.tsx
@@ -3,6 +3,7 @@ import ControlButton from './control-button';
 import { Slider } from '@/components/ui/slider';
 import { selectPlayerControllerStates, useAppDispatch, useTypedSelector } from '@/store';
 import { togglePlay, toggleRepeat, toggleShuffle } from '@/features/player-controller/player-controller-slice';
+import { useEffect, useState } from 'react';
 
 const onPrevious = () => {
   console.log('Previous Song');
@@ -15,11 +16,27 @@ export default function PlayerController(): JSX.Element {
   const { isPlaying, isShuffle, isRepeat } = useTypedSelector(selectPlayerControllerStates);
   const dispatch = useAppDispatch();
 
+  const [currentTime, setCurrentTime] = useState<number>(0);
+
   const iconProperty = { strokeWidth: 2.5, size: 20 };
 
   const onPlay = () => dispatch(togglePlay());
   const onShuffle = () => dispatch(toggleShuffle());
   const onRepeat = () => dispatch(toggleRepeat());
+
+  useEffect(() => {
+    if (isPlaying) {
+      const interval = setInterval(() => {
+        setCurrentTime((state) => state + 1);
+      }, 1000);
+
+      return () => clearInterval(interval);
+    }
+  }, [isPlaying]);
+
+  const onSliderChange = (value: number[]) => {
+    setCurrentTime(value[0]);
+  };
 
   return (
     <div className="flex w-full max-w-[35vw] flex-col items-center gap-1">
@@ -36,7 +53,7 @@ export default function PlayerController(): JSX.Element {
           onClick={onPlay}
           className="bg-zinc-100 text-black transition-transform duration-100 hover:scale-105 active:scale-95"
         >
-          {isPlaying ? <PlayIcon className="relative left-[1px]" {...iconProperty} /> : <PauseIcon {...iconProperty} />}
+          {isPlaying ? <PauseIcon {...iconProperty} /> : <PlayIcon className="relative left-[1px]" {...iconProperty} />}
         </ControlButton>
 
         <ControlButton type="button" onClick={onNext}>
@@ -55,9 +72,12 @@ export default function PlayerController(): JSX.Element {
       </div>
 
       <div className="flex w-full flex-row items-center gap-2 text-zinc-200">
-        <p className="text-xs text-zinc-400">0:00</p>
-        <Slider defaultValue={[1]} min={0} max={100} step={1} />
-        <p className="text-xs text-zinc-400">6:53</p>
+        <p className="text-xs text-zinc-400">{`${Math.floor(currentTime / 60)}:${String(currentTime % 60).padStart(
+          2,
+          '0',
+        )}`}</p>
+        <Slider defaultValue={[1]} min={0} value={[currentTime]} onValueChange={onSliderChange} max={420} step={1} />
+        <p className="text-xs text-zinc-400">7:00</p>
       </div>
     </div>
   );
