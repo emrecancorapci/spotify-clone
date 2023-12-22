2407f61a6ff2a02469027310c00e1c0777f3a8c4
Emre Can Çorapçı <emrecancorapci@gmail.com>
2023-12-23 00:53:48 +0300

    fix time display flickering

--- src/components/layout/footer/time-display.tsx
+++ src/components/layout/footer/time-display.tsx
@@ -1,9 +1,9 @@
-const TimeDisplay = ({ seconds }: { seconds: number }) => {
-  // Calculate minutes and remaining seconds
+import { memo } from 'react';
+
+const TimeDisplay = memo(({ seconds }: { seconds: number }) => {
   const minutes = Math.floor(seconds / 60);
   const remainingSeconds = seconds % 60;
 
-  // Ensure that minutes and seconds are displayed with leading zeros if needed
   const formattedMinutes = String(minutes).padStart(2, '0');
   const formattedSeconds = String(Math.round(remainingSeconds)).padStart(2, '0');
 
@@ -12,6 +12,8 @@ const TimeDisplay = ({ seconds }: { seconds: number }) => {
       {formattedMinutes}:{formattedSeconds}
     </p>
   );
-};
+});
+
+TimeDisplay.displayName = 'TimeDisplay';
 
 export default TimeDisplay;
