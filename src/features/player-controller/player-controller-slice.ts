8a6529b36cd8a061b62d340c5f3646f40a523c59
Emre Can Çorapçı <emrecancorapci@gmail.com>
2023-12-23 00:33:29 +0300

    add new states and reducers to player controller slice

--- src/features/player-controller/player-controller-slice.ts
+++ src/features/player-controller/player-controller-slice.ts
@@ -1,9 +1,10 @@
 import { createSlice, type PayloadAction, type UnknownAction } from '@reduxjs/toolkit';
-import { type RefObject } from 'react';
 
 export interface PlayerControllerState {
-  audioReference: RefObject<HTMLAudioElement> | undefined;
+  audioSource: string | undefined;
   volume: number;
+  duration: number;
+  currentTime: number;
   isMuted: boolean;
   isPlaying: boolean;
   isShuffle: boolean;
@@ -11,8 +12,10 @@ export interface PlayerControllerState {
 }
 
 const initialState: PlayerControllerState = {
-  audioReference: undefined,
+  audioSource: undefined,
   volume: 0.5,
+  duration: 0,
+  currentTime: 0,
   isMuted: false,
   isPlaying: false,
   isShuffle: false,
@@ -23,21 +26,36 @@ function isActionWithNumberPayload(action: UnknownAction): action is PayloadActi
   return typeof action.payload === 'number';
 }
 
+function isActionWithStringPayload(action: UnknownAction): action is PayloadAction<string> {
+  return typeof action.payload === 'string';
+}
+
 export const playerControllerSlice = createSlice({
   name: 'playerController',
   initialState,
   reducers: {
-    setAudioReference: (state, action) => {
-      // Fix type
-      state.audioReference = action.payload;
+    setAudioSource(state, action) {
+      if (!isActionWithStringPayload(action)) return;
+
+      state.audioSource = action.payload;
     },
     setVolume: (state, action) => {
-      if (isActionWithNumberPayload(action)) {
-        if (state.isMuted) {
-          state.isMuted = false;
-        }
-        state.volume = action.payload;
+      if (!isActionWithNumberPayload(action)) return;
+
+      if (state.isMuted) {
+        state.isMuted = false;
       }
+      state.volume = action.payload;
+    },
+    setCurrentTime: (state, action: PayloadAction<number>) => {
+      if (!isActionWithNumberPayload(action)) return;
+
+      state.currentTime = action.payload;
+    },
+    setDuration: (state, action: PayloadAction<number>) => {
+      if (!isActionWithNumberPayload(action)) return;
+
+      state.duration = action.payload;
     },
     toggleMute: (state) => {
       state.isMuted = !state.isMuted;
@@ -68,7 +86,15 @@ export const playerControllerSlice = createSlice({
   },
 });
 
-export const { setAudioReference, setVolume, togglePlay, toggleMute, toggleShuffle, toggleRepeat } =
-  playerControllerSlice.actions;
+export const {
+  setAudioSource,
+  setVolume,
+  setDuration,
+  setCurrentTime,
+  togglePlay,
+  toggleMute,
+  toggleShuffle,
+  toggleRepeat,
+} = playerControllerSlice.actions;
 
 export default playerControllerSlice.reducer;
--- src/store.ts
+++ src/store.ts
@@ -11,11 +11,15 @@ export const store = configureStore({
   },
 });
 
+const audioSource = (state: RootState) => state.playerController.audioSource;
+const volume = (state: RootState) => state.playerController.volume;
+const duration = (state: RootState) => state.playerController.duration;
+const currentTime = (state: RootState) => state.playerController.currentTime;
+
 const isPlaying = (state: RootState) => state.playerController.isPlaying;
+const isMuted = (state: RootState) => state.playerController.isMuted;
 const isShuffle = (state: RootState) => state.playerController.isShuffle;
 const isRepeat = (state: RootState) => state.playerController.isRepeat;
-const volume = (state: RootState) => state.playerController.volume;
-const isMuted = (state: RootState) => state.playerController.isMuted;
 
 export const selectButtonGroupStates = createSelector(
   [isPlaying, isShuffle, isRepeat],
@@ -27,6 +31,12 @@ export const selectButtonGroupStates = createSelector(
 export const selectOtherControlsStates = createSelector([volume, isMuted], (volume, isMuted) => {
   return { volume, isMuted };
 });
+export const selectAudioPlayerStates = createSelector(
+  [audioSource, volume, isMuted, isPlaying],
+  (audioSource, volume, isMuted, isPlaying) => {
+    return { audioSource, volume, isMuted, isPlaying };
+  },
+);
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
