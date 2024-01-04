b0822b3013e7452327d30ae2a45c5471e2824521
Emre Can Çorapçı <emrecancorapci@gmail.com>
2024-01-05 01:31:59 +0300

    refactor library-card component to use tooltip wrapper

--- src/components/layout/sidebar/library-card.tsx
+++ src/components/layout/sidebar/library-card.tsx
@@ -1,5 +1,7 @@
 import { PinIcon } from 'lucide-react';
 
+import TooltipWrapper from '@/components/ui/tooltip-wrapper';
+
 interface Properties {
   library: {
     name: string;
@@ -15,20 +17,33 @@ export default function LibraryCard({
   isPinned,
   isCollapsed = true,
 }: Properties): JSX.Element {
-  const iconProperties = { strokeWidth: 2.5, size: 26 };
+  const LibraryImage = () => (
+    <div className="h-12 w-12 shrink-0 rounded bg-s-gray" style={{ backgroundImage: `url(${image})` }} />
+  );
 
-  return (
-    <div className="rounded-lg p-2 transition-colors duration-300 hover:bg-zinc-800">
+  const LibraryInfo = () => (
+    <div className="flex flex-col">
+      <div className="text-base leading-none">{name}</div>
+      <div className="flex flex-row items-end gap-2 py-1">
+        {isPinned ? <PinIcon className="text-s-green" strokeWidth={2} size={14} /> : undefined}{' '}
+        <p className="text-base leading-none text-s-gray-light">{songCount} songs</p>
+      </div>
+    </div>
+  );
+
+  return isCollapsed ? (
+    <TooltipWrapper sideOffset={12} side="right" tooltipContent={<LibraryInfo />}>
+      <div className="flex items-center rounded-lg p-2 transition-colors duration-300 hover:bg-s-gray-darker">
+        <LibraryImage />
+      </div>
+    </TooltipWrapper>
+  ) : (
+    <div className="rounded-lg p-2 transition-colors duration-300 hover:bg-s-gray-dark">
       <div className="flex items-center gap-2">
-        <div className="h-12 w-12 shrink-0 rounded bg-zinc-700" style={{ backgroundImage: `url(${image})` }} />
-        {isCollapsed ? undefined : (
-          <div className="flex flex-col">
-            <div className="text-sm font-semibold">{name}</div>
-            <div className="text-xs text-zinc-500">
-              {isPinned ? <PinIcon {...iconProperties} /> : undefined} {songCount} songs
-            </div>
-          </div>
-        )}
+        <LibraryImage />
+        <div className="flex flex-col">
+          <LibraryInfo />
+        </div>
       </div>
     </div>
   );
