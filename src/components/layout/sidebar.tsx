9149e49544647d8c48b819053a308c1f2c649843
Emre Can Çorapçı <emrecancorapci@gmail.com>
2024-01-07 19:03:36 +0300

    add scrolling to sidebar libraries area

--- src/components/layout/sidebar.tsx
+++ src/components/layout/sidebar.tsx
@@ -1,7 +1,8 @@
 import { HomeIcon, LibraryBig, SearchIcon } from 'lucide-react';
 import { NavLink } from 'react-router-dom';
 
-import TooltipWrapper from '../ui/tooltip-wrapper';
+import TooltipWrapper from '@/components/ui/tooltip-wrapper';
+
 import LibraryCard from './sidebar/library-card';
 import SidebarCard from './sidebar/sidebar-card';
 
@@ -9,7 +10,7 @@ export default function Sidebar(): JSX.Element {
   const iconProperties = { strokeWidth: 2, size: 26 };
 
   return (
-    <div className="flex w-full flex-col gap-2">
+    <div className="flex max-h-full w-full flex-col gap-2">
       <SidebarCard>
         <NavLink to="/">
           {({ isActive }) => (
@@ -36,17 +37,29 @@ export default function Sidebar(): JSX.Element {
           )}
         </NavLink>
       </SidebarCard>
-      <SidebarCard>
-        <TooltipWrapper tooltipContent="Expand Your Library" side="right">
-          <LibraryBig
-            className="text-s-gray-light transition-colors duration-300 hover:text-white"
-            {...iconProperties}
-          />
-        </TooltipWrapper>
+      <SidebarCard className="overflow-hidden">
         <div>
+          <TooltipWrapper tooltipContent="Expand Your Library" side="right">
+            <LibraryBig
+              className="text-s-gray-light transition-colors duration-300 hover:text-white"
+              {...iconProperties}
+            />
+          </TooltipWrapper>
+        </div>
+        <div className="flex flex-col overflow-y-scroll ps-4">
           <LibraryCard library={{ name: 'Liked Songs', songCount: 0 }} isPinned={true} />
           <LibraryCard library={{ name: 'Heavy Metal', songCount: 0 }} isPinned={true} />
           <LibraryCard library={{ name: 'Jazz', songCount: 0 }} isPinned={false} />
+          <LibraryCard library={{ name: 'Funk', songCount: 0 }} isPinned={false} />
+          <LibraryCard library={{ name: 'Soul', songCount: 0 }} isPinned={false} />
+          <LibraryCard library={{ name: 'Classic Rock', songCount: 0 }} isPinned={false} />
+          <LibraryCard library={{ name: 'Progressive Rock', songCount: 0 }} isPinned={false} />
+          <LibraryCard library={{ name: 'Death Metal', songCount: 0 }} isPinned={false} />
+          <LibraryCard library={{ name: 'Fusion Jazz', songCount: 0 }} isPinned={false} />
+          <LibraryCard library={{ name: 'Neo-Soul', songCount: 0 }} isPinned={false} />
+          <LibraryCard library={{ name: 'Black Metal', songCount: 0 }} isPinned={false} />
+          <LibraryCard library={{ name: 'R&B', songCount: 0 }} isPinned={false} />
+          <LibraryCard library={{ name: 'Indie Rock', songCount: 0 }} isPinned={false} />
         </div>
       </SidebarCard>
     </div>
--- src/components/layout/sidebar/sidebar-card.tsx
+++ src/components/layout/sidebar/sidebar-card.tsx
@@ -1,11 +1,13 @@
 interface Properties {
+  className?: string;
   children: React.ReactNode | React.ReactNode[];
-  gap?: number;
 }
 
-export default function SidebarCard({ children }: Properties): JSX.Element {
+export default function SidebarCard({ className, children }: Properties): JSX.Element {
   return (
-    <div className={`flex w-full flex-col items-center gap-6 rounded-lg bg-s-gray-darkest py-4 text-white`}>
+    <div
+      className={`flex w-full flex-col items-center gap-6 rounded-lg bg-s-gray-darkest py-4 text-white ${className}`}
+    >
       {children}
     </div>
   );
