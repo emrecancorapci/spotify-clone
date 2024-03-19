ad1a291e2d8e635c3b3eeb9ae3c694b8cd03071b
Emre Can Çorapçı <emrecancorapci@gmail.com>
2024-03-19 15:25:48 +0300

    Update build options, Add lightningcss and terser for CSS and JS minification

--- vite.config.ts
+++ vite.config.ts
@@ -6,24 +6,15 @@ import tsConfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   plugins: [react(),  eslintPlugin(), tsConfigPaths()],
-  // prevent vite from obscuring rust errors
-  clearScreen: false,
-  // Tauri expects a fixed port, fail if that port is not available
-  server: {
-    strictPort: true,
-  },
-  // to make use of `TAURI_PLATFORM`, `TAURI_ARCH`, `TAURI_FAMILY`,
-  // `TAURI_PLATFORM_VERSION`, `TAURI_PLATFORM_TYPE` and `TAURI_DEBUG`
-  // env variables
-  // Fix after @tauri-apps/cli is updated
-  envPrefix: ['VITE_'],
   build: {
-    // Tauri uses Chromium on Windows and WebKit on macOS and Linux
-    target: process.env.TAURI_PLATFORM == 'windows' ? 'chrome105' : 'safari13',
-    // don't minify for debug builds
-    minify: !process.env.TAURI_DEBUG ? 'esbuild' : false,
-    // produce sourcemaps for debug builds
-    sourcemap: !!process.env.TAURI_DEBUG,
+    cssMinify: "lightningcss",
+    minify: "terser",
+    terserOptions: {
+      compress: {
+        drop_console: true
+      },
+      module: true
+    }
   },
   resolve: {
     alias: {
