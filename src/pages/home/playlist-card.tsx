d5e3864d0bf522a90d404fcded62fe37a919d59b
Emre Can Çorapçı <emrecancorapci@gmail.com>
2024-03-20 01:46:39 +0300

    Add playlist card

---
+++ src/pages/home/playlist-card.tsx
@@ -0,0 +1,31 @@
+interface Properties {
+  image?: string;
+  title: string;
+  description: string;
+}
+
+const truncateStyle: React.CSSProperties = {
+  WebkitLineClamp: 2,
+  display: '-webkit-box',
+  WebkitBoxOrient: 'vertical',
+  overflow: 'hidden',
+  textOverflow: 'ellipsis',
+};
+
+export default function PlaylistCard({ image, title, description }: Properties) {
+  return (
+    <div className="flex flex-col rounded-lg bg-transparent p-3 hover:bg-s-gray/20">
+      <img
+        className="w-full rounded-lg object-cover"
+        src={image ?? 'https://via.placeholder.com/256'}
+        height={256}
+        width={256}
+        alt="playlist"
+      />
+      <p className="max-w-full truncate py-3 ps-2 leading-none text-white">{title}</p>
+      <p style={truncateStyle} className="ps-2 text-sm leading-tight text-s-gray-lighter">
+        {description}
+      </p>
+    </div>
+  );
+}
