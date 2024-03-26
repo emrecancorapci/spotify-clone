a86be1a89af03bd893df95eb7ae539f5cb32cee6
Emre Can Çorapçı <emrecancorapci@gmail.com>
2024-03-27 01:13:40 +0300

    Add play button, update playlist card for including play button

---
+++ src/components/ui/play-button.tsx
@@ -0,0 +1,32 @@
+import { cva, type VariantProps } from 'class-variance-authority';
+import { PlayIcon } from 'lucide-react';
+
+import { cn } from '@/lib/utils';
+
+const buttonVariants = cva(
+  'flex items-center justify-center rounded-full bg-s-green text-black hover:bg-s-green-light',
+  {
+    variants: {
+      size: {
+        sm: 'size-[40px] hover:size-[41px]',
+        md: 'size-[48px] hover:size-[49px]',
+        lg: 'size-[56px] hover:size-[57px]',
+      },
+    },
+    defaultVariants: {
+      size: 'md',
+    },
+  },
+);
+
+interface ButtonProperties extends React.ButtonHTMLAttributes<HTMLButtonElement>, VariantProps<typeof buttonVariants> {
+  asChild?: boolean;
+}
+
+export default function PlayButton({ className, size, ...properties }: ButtonProperties): React.ReactNode {
+  return (
+    <button className={cn(buttonVariants({ size, className }))} {...properties}>
+      <PlayIcon size={24} />
+    </button>
+  );
+}
--- src/pages/home/playlist-card.tsx
+++ src/pages/home/playlist-card.tsx
@@ -1,8 +1,14 @@
+import { useCallback, useState } from 'react';
+
+import PlayButton from '@/components/ui/play-button';
+
 interface Properties {
   image?: string;
   title: string;
   description: string;
 }
+
+const buttonSizePx = 48;
 
 const truncateStyle: React.CSSProperties = {
   WebkitLineClamp: 2,
@@ -12,15 +18,38 @@ const truncateStyle: React.CSSProperties = {
   textOverflow: 'ellipsis',
 };
 
+function getButtonPositionStyle(imageSize: number): React.CSSProperties {
+  return {
+    top: `calc(${imageSize}px - ${buttonSizePx - 4}px)`,
+    left: `calc(${imageSize}px - ${buttonSizePx - 4}px)`,
+  };
+}
+
 export default function PlaylistCard({ image, title, description }: Properties) {
+  const [imageSize, setImageSize] = useState<number>(0);
+
+  const imageReference = useCallback(function sizeChangeHandler(node: HTMLImageElement) {
+    if (!node) return;
+    const resizeObserver = new ResizeObserver(() => {
+      setImageSize(node.clientWidth);
+    });
+
+    resizeObserver.observe(node);
+  }, []);
+
   return (
-    <div className="flex flex-col rounded-lg bg-transparent p-3 hover:bg-s-gray/20">
+    <div className="group relative flex flex-col rounded-lg bg-transparent p-3 hover:cursor-pointer hover:bg-s-gray/20">
       <img
+        ref={imageReference}
         className="w-full rounded-lg object-cover shadow-lg shadow-black/50"
         src={image ?? 'https://via.placeholder.com/256'}
         height={256}
         width={256}
         alt="playlist"
+      />
+      <PlayButton
+        style={getButtonPositionStyle(imageSize)}
+        className="absolute translate-y-2 opacity-0 shadow transition-all duration-300 ease-in group-hover:translate-y-0 group-hover:opacity-100"
       />
       <p className="max-w-full truncate py-3 ps-2 leading-none text-white">{title}</p>
       <p style={truncateStyle} className="ps-2 text-sm leading-tight text-s-gray-lighter">
