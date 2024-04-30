3b4ee0c01ee2e9fbb62576a42f794e412ac0472c
Emre Can Çorapçı <emrecancorapci@gmail.com>
2024-05-01 01:09:39 +0300

    Refactor CardGroup to DynamicGrid and PlaylistCard to ItemCard and add more functionality

---
+++ src/components/ui/dynamic-grid.tsx
@@ -0,0 +1,69 @@
+import { Link } from 'react-router-dom';
+
+import { useAppControllerStore } from '@/features/appControllerStore';
+import { cn } from '@/lib/utils';
+
+interface OrderableAndIndexable {
+  id: number | string;
+  order: number;
+}
+
+interface Properties<T extends OrderableAndIndexable> {
+  className?: string;
+  title: string;
+  description?: string;
+  items: T[];
+  to?: string;
+  Component: (properties: T) => React.ReactNode;
+}
+
+const cardSizePx = 220;
+
+export default function DynamicGrid<T>({
+  className,
+  title,
+  description,
+  items,
+  to,
+  Component,
+}: Properties<T extends OrderableAndIndexable ? T : never>): React.ReactNode {
+  const width = useAppControllerStore((state) => state.mainWidth);
+
+  const columnCount = Math.floor(width / cardSizePx);
+
+  return (
+    <div>
+      <div className={cn('flex w-full items-center justify-between px-3.5 py-2', className)}>
+        <Link to={to ?? '/'}>
+          <button type="button" className="text-2xl font-bold text-white no-underline hover:underline">
+            {title}
+          </button>
+        </Link>
+        <Link to={to ?? '/'}>
+          <button
+            type="button"
+            className="box-decoration-slice text-sm font-bold text-s-gray-lighter no-underline hover:underline"
+          >
+            Show all
+          </button>
+        </Link>
+      </div>
+      {description && <p className="text-sm text-s-gray-lighter">{description}</p>}
+      <div style={getGridStyle(columnCount)}>
+        {items
+          .slice(0, columnCount)
+          .sort((a, b) => a.order - b.order)
+          .map((playlist) => (
+            <Component key={playlist.id} {...playlist} />
+          ))}
+      </div>
+    </div>
+  );
+}
+
+function getGridStyle(columnCount: number): React.CSSProperties {
+  return {
+    display: 'grid',
+    gridTemplateColumns: `repeat(${columnCount}, minmax(0, 1fr))`,
+  };
+}
--- src/components/ui/item-card.tsx
+++ src/components/ui/item-card.tsx
@@ -1,3 +1,4 @@
+import { cva } from 'class-variance-authority';
 import { useCallback, useState } from 'react';
 
 import PlayButton from '@/components/ui/play-button';
@@ -5,10 +6,23 @@ import PlayButton from '@/components/ui/play-button';
 interface Properties {
   image?: string;
   title: string;
-  description: string;
+  followers?: string | number;
+  description?: string;
+  isArtist?: boolean;
+  showFollowers?: boolean;
 }
 
-const buttonSizePx = 48;
+const imageVariants = cva('aspect-square w-full object-cover shadow-lg shadow-black/50', {
+  variants: {
+    isArtist: {
+      true: 'rounded-full',
+      false: 'rounded-lg',
+    },
+  },
+  defaultVariants: {
+    isArtist: false,
+  },
+});
 
 const truncateStyle: React.CSSProperties = {
   WebkitLineClamp: 2,
@@ -18,14 +32,7 @@ const truncateStyle: React.CSSProperties = {
   textOverflow: 'ellipsis',
 };
 
-function getButtonPositionStyle(imageSize: number): React.CSSProperties {
-  return {
-    top: `calc(${imageSize}px - ${buttonSizePx - 4}px)`,
-    left: `calc(${imageSize}px - ${buttonSizePx - 4}px)`,
-  };
-}
-
-export default function PlaylistCard({ image, title, description }: Properties): React.ReactNode {
+export default function ItemCard({ image, title, description, isArtist }: Properties): React.ReactNode {
   const [imageSize, setImageSize] = useState<number>(0);
 
   const imageReference = useCallback(function sizeChangeHandler(node: HTMLImageElement) {
@@ -38,10 +45,10 @@ export default function PlaylistCard({ image, title, description }: Properties):
   }, []);
 
   return (
-    <div className="group relative flex flex-col rounded-lg bg-transparent p-3 hover:cursor-pointer hover:bg-s-gray/20">
+    <div className="group relative flex flex-col rounded-lg bg-transparent p-3 hover:cursor-pointer hover:bg-white/10">
       <img
         ref={imageReference}
-        className="w-full rounded-lg object-cover shadow-lg shadow-black/50"
+        className={imageVariants({ isArtist })}
         src={image ?? 'https://via.placeholder.com/256'}
         height={256}
         width={256}
@@ -51,9 +58,21 @@ export default function PlaylistCard({ image, title, description }: Properties):
         <PlayButton className="translate-y-2 opacity-0 shadow transition-[opacity,transform] duration-200 ease-in group-hover:translate-y-0 group-hover:opacity-100" />
       </div>
       <p className="max-w-full truncate pb-2 ps-2 pt-3 leading-none text-white">{title}</p>
-      <p style={truncateStyle} className="ps-2 text-sm leading-tight text-s-gray-lighter">
-        {description}
-      </p>
+      {description ? (
+        <p style={truncateStyle} className="ps-2 text-sm leading-tight text-s-gray-lighter">
+          {description}
+        </p>
+      ) : (
+        <div className="h-5"></div>
+      )}
     </div>
   );
+}
+
+function getButtonPositionStyle(imageSize: number): React.CSSProperties {
+  const buttonSizePx = 48;
+  return {
+    top: `calc(${imageSize}px - ${buttonSizePx - 4}px)`,
+    left: `calc(${imageSize}px - ${buttonSizePx - 4}px)`,
+  };
 }
